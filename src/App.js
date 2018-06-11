f31bc6e8b65f05cfcfb240a933bec3eb37001e75
QzSG <quantumparadoxes@gmail.com>
2018-06-11 16:02:30 +0800

    Add Site Title

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import { Container } from 'reactstrap';
+import { Helmet } from 'react-helmet';
 //import logo from './logo.svg';
 import './styles/App.css';
 import Nav from './components/Nav';
@@ -12,6 +13,11 @@ class App extends Component {
     return (
       <Router>  
         <Container fluid className="App d-flex flex-column">
+          <Helmet>
+            <title>QzSG | Home</title>
+            <meta name="description" content="Welcome to QzSG personal website!" />
+          </Helmet>
+
           <Nav />
             
           <Route exact path="/" component={Home} />
