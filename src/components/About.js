a088c291e0ca07954180e0a83bc956084d6fe125
QzSG <quantumparadoxes@gmail.com>
2018-06-12 00:26:24 +0800

    Add about

--- src/components/About.js
+++ src/components/About.js
@@ -1,19 +1,40 @@
 import React, {Component} from 'react';
-import { Row, Col } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
+import { Card, CardBody, CardText } from 'reactstrap';
 import {Helmet} from 'react-helmet';
-import './../styles/Home.css';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import faHeart from '@fortawesome/fontawesome-free-regular/faHeart';
+import {faCoffee,faCode} from '@fortawesome/fontawesome-free-solid';
+import './../styles/About.css';
+import avatar from './../images/me.svg';
 
 class Home extends Component {
   render() {
     return (
-        <Row className="text-white justify-content-center align-items-center mb-0 mx-0 d-flex flex-grow-1">
-          <Helmet>
-            <title>QzSG | About</title>
-            <meta name="description" content="Know more about QzSG!" />
-          </Helmet>
-          <Row className="justify-content-center">
-            <Col md="12" sm="12" className="text-justify"><h2>Hey!</h2></Col>
-        </Row></Row>
+      <Row className="text-dark justify-content-center align-items-center mb-0 mx-0 d-flex flex-grow-1 bg-custom">
+      <Helmet>
+        <title>QzSG | About</title>
+        <meta name="description" content="Learn more about QzSG!" />
+      </Helmet>
+      <Row className="align-self-center justify-content-center">
+        <Card body outline className="col-10 col-sm-10 col-md-8 col-lg-8 col-xl-10">
+            <CardBody>
+            <Row className='justify-content-center'>
+              <Col xs='9' sm='8' md='6'  lg='5'>
+                <img src={avatar} className="rounded-circle img-fluid" />
+              </Col>
+            </Row>
+            </CardBody>
+            <CardText tag='h3' style={{ fontFamily:'Raleway'}} >Adrian Tan</CardText>
+            <CardText tag='p' className="lead">Technology Ninja</CardText>
+            <CardText tag='p' className="text-justify align-self-center">
+            <FontAwesomeIcon icon={faHeart} size=""/> playing with the latest technology and reading about the latest science & technology innovations.<br/>
+            Likes to read books and listen to music when bored. Turns copious amounts of <FontAwesomeIcon icon={faCoffee} size=""/> into awesome <FontAwesomeIcon icon={faCode} size=""/> !
+            </CardText>
+        </Card>
+      </Row>
+      
+    </Row>
     );
   }
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { NavLink as Link } from 'react-router-dom';
 import {
-    Container,
     Collapse,
     Navbar,
     NavbarToggler,
