30386acf0e63f3f41be0be188f2cc15fcc48ef05
QzSG <quantumparadoxes@gmail.com>
2018-06-16 22:45:36 +0800

    Fix Meta Tags

--- src/components/FourOhFour.js
+++ src/components/FourOhFour.js
@@ -14,8 +14,8 @@ class FourOhFour extends Component {
     return (
         <Row className="text-white justify-content-center align-items-center mb-0 mx-0 d-flex flex-grow-1 bg-custom">
           <Helmet>
-            <title>QzSG | Home</title>
-            <meta name="description" content="Welcome to QzSG personal website!" />
+            <title>QzSG | Time Travel Prohibited</title>
+            <meta name="description" content="Time Travel Prohibited!" />
           </Helmet>
           <Row className="justify-content-center align-items-center">
             <Col md="5" sm="12" className="text-justify"><h3>Sorry, your destination does not exist in this universe!</h3></Col>
